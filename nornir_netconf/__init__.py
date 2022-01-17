53d6734ff861d8c1a088f7ea49b076503840c8b4
Hugo Tinoco <htinoco@devpop.localdomain>
2022-01-17 16:54:52 -0700

    metadata

--- nornir_netconf/__init__.py
+++ nornir_netconf/__init__.py
@@ -1,2 +1,9 @@
 """Nornir NETCONF Plugin."""
-__version__ = "0.1.0"
+try:
+    from importlib import metadata  # type: ignore
+except ImportError:
+    # Python version < 3.8
+    import importlib_metadata as metadata  # type: ignore
+
+# This will read version from pyproject.toml
+__version__ = metadata.version("nornir_netconf")
